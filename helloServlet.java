e4564afe4686ca0ac340283c89650bd37380ea77
holson1 <henryolson1@gmail.com>
2014-02-21 15:13:49 -0800

    Committed by qcommit.sh

--- helloServlet.java
+++ helloServlet.java
@@ -1,20 +1,25 @@
+//	Henry Olson
+//	helloServlet.java
+//	A servlet that accepts a "name" from POST and prints it back out to the user
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class helloServlet extends HttpServlet {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
+	//handles POST
 	public void doPost(HttpServletRequest request,
 			HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		//prepare writer
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		
+		//standard html stuff
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<head>");
@@ -24,14 +29,18 @@ public class helloServlet extends HttpServlet {
 		out.println("<body>");
 		out.println("<h1>First Web Application</h1>");
 		
+		//get name from POST
 		String name = request.getParameter("name");
 		
-		if(name != null)
+		//check for input and print message accordingly
+		if(!name.isEmpty())
 			out.println("Hi there, " + name + "!\n");
 		else
 			out.println("You didn't enter any text\n");
 		
 		out.println("<br/>");
+		
+		//link to return
 		out.println("<a href=\"../../index.html\">"
 				+ "Return to form</a>");
 		
